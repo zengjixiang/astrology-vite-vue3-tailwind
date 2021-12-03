89e8263f99bd78e175d88beb24f68bd6d790991b
zengjixiang <zengjixiang0401@gmail.com>
2021-12-03 20:22:36 +0800

    修改引號和設定

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,20 @@
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-import NotFound from './views/NotFound.vue'
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import NotFound from "./views/NotFound.vue";
 
 /** @type {import('vue-router').RouterOptions['routes']} */
+/** Routes (路由配置)
+ */
 export const routes = [
-  { path: '/', component: Home, meta: { title: '系統首頁' } },
+  { path: "/", component: Home, meta: { title: "系統首頁" } },
   {
-    path: '/about',
-    meta: { title: '關於作者' },
+    path: "/about",
+    meta: { title: "關於作者" },
     component: About,
     // example of route level code-splitting
     // this generates a separate chunk (About.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import('./views/About.vue')
   },
-  { path: '/:path(.*)', component: NotFound },
-]
+  { path: "/:path(.*)", component: NotFound },
+];
